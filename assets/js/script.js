1657bb252c0cb21a1c90b9a3dbab8c9b8ad49a1f
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-09 17:00:50 +0200

    [add] feat button.addEvenListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,17 @@ const button = document.getElementById('interruttore');
 const lampOff = document.getElementById('lampOff');
 const lampOn = document.getElementById('lampOn');
 
-
+button.addEventListener('click', function () {
+  if (lampOff.classList.contains('display-inline-block')) {
+    lampOff.classList.add('display-none');
+    lampOff.classList.remove('display-inline-block');
+    lampOn.classList.add('display-inline-block');
+    lampOn.classList.remove('display-none');
+  }
+  else {
+    lampOff.classList.remove('display-none');
+    lampOff.classList.add('display-inline-block');
+    lampOn.classList.remove('display-inline-block');
+    lampOn.classList.add('display-none');
+  }
+});
